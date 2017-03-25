cd39b118e3660eb8da8aae3dafea475223aac8d6
Lark Mullins <larkmullins@gmail.com>
2017-03-25 10:57:24 -0400

    updated Generate func, added GenerateInt func, and updated tests

--- micra.go
+++ micra.go
@@ -1,44 +1,61 @@
 package micra
 
-import "math/rand"
-import "time"
-import "strings"
+import (
+	"errors"
+	"math/rand"
+	"strconv"
+	"strings"
+	"time"
+)
 
-var glyphs string = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+var glyphs = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+var numbers = "1234567890"
 
-func Generate(l int, g ...string) (string) {
-    if l < 5 {
-        panic("Length must be more or equal to 5")
-    }
+// Generate will create a new string
+func Generate(l int, g ...string) (string, error) {
+	if l < 5 {
+		return "", errors.New("Length must be more or equal to 5")
+	}
 
-    if g != nil {
-        glyphs = strings.Join(g, "")
-    }
+	if g != nil {
+		glyphs = strings.Join(g, "")
+	}
 
-    if len(glyphs) < l {
-        panic("Length of ID cannot be longer than glyphs")
-    }
+	if len(glyphs) < l {
+		return "", errors.New("Length of ID cannot be longer than glyphs (62)")
+	}
 
-    a := split(glyphs)
-    s := shuffle(a)
+	a := split(glyphs)
+	s := shuffle(a)
 
-    return strings.Join(slice(s, l), "")
+	return strings.Join(slice(s, l), ""), nil
+}
+
+// GenerateInt will create a new random integer
+func GenerateInt(l int) (int64, error) {
+	s, err := Generate(l, "0123456789")
+
+	if err != nil {
+		return 0, err
+	}
+
+	return strconv.ParseInt(s, 10, 0)
 }
 
 func shuffle(a []string) []string {
-    rand.Seed(time.Now().UnixNano())
-    for i := range a {
-        j := rand.Intn(i + 1)
-        a[i], a[j] = a[j], a[i]
-    }
+	rand.Seed(time.Now().UnixNano())
+	for i := range a {
+		j := rand.Intn(i + 1)
+		a[i], a[j] = a[j], a[i]
+	}
 
-    return a
+	return a
 }
 
 func split(a string) []string {
-    return strings.Split(a, "")
+	return strings.Split(a, "")
 }
 
 func slice(a []string, l int) []string {
-    return a[0:l]
+	return a[0:l]
 }
--- micra_test.go
+++ micra_test.go
@@ -1,13 +1,51 @@
 package micra
 
 import (
-    "testing"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func Test_Generate_length_is_10(t *testing.T) {
-    if (len(Generate(10)) != 10) {
-        t.Error("Generate did not return string with length of 10")
-    } else {
-        t.Log("Test_Generate_length_is_10 passed")
-    }
+func TestGenerateLengthIs10(t *testing.T) {
+	s, _ := Generate(10)
+
+	assert.Len(t, s, 10)
+}
+
+func TestItErrorsWhenLengthIsLessThanFive(t *testing.T) {
+	_, err := Generate(4)
+
+	if assert.Error(t, err) {
+		assert.Equal(t, "Length must be more or equal to 5", err.Error())
+	}
+}
+
+func TestItErrorsWhenLengthIsLargerThanDefaultGlyphs(t *testing.T) {
+	_, err := Generate(63)
+
+	if assert.Error(t, err) {
+		assert.Equal(t, "Length of ID cannot be longer than glyphs (62)", err.Error())
+	}
+}
+
+func TestAddingCustomGlyphs(t *testing.T) {
+	s, _ := Generate(5, "aaaaa")
+
+	if assert.Len(t, s, 5) {
+		assert.Equal(t, "aaaaa", s)
+	}
+}
+
+func TestGenerateInt(t *testing.T) {
+	s, err := GenerateInt(5)
+
+	if assert.NoError(t, err) {
+		assert.IsType(t, new(int64), &s)
+	}
+}
+
+func TestGenerateIntErrors(t *testing.T) {
+	_, err := GenerateInt(4)
+
+	assert.Error(t, err)
 }
